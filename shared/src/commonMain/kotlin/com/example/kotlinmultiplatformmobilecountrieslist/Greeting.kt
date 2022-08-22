9faa471b3ea6a09a7e0650bb4840004152089818
boris <boris.kunda@entrypoint.org.il>
2022-08-22 11:04:32 +0300

    * initial

---
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformmobilecountrieslist/Greeting.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinmultiplatformmobilecountrieslist
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformmobilecountrieslist/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinmultiplatformmobilecountrieslist
+
+expect class Platform() {
+    val platform: String
+}
